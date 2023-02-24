4636cc0af09b2aba4835993e000d4a5a4eb6ee4a
Max Rusakovič <maxrusakovic@gmail.com>
2023-02-24 23:04:48 +0100

    [18] Removed cache wrapper from fetch function

--- src/utils/fetch/getCharacters.ts
+++ src/utils/fetch/getCharacters.ts
@@ -1,20 +1,20 @@
-import { cache } from "react";
+export const getCharacters = async (
+  page: number = 1,
+  gender?: string,
+  name?: string
+) => {
+  var url = new URL("https://rickandmortyapi.com/api/character/");
+  page && url.searchParams.set("page", page.toString());
+  gender && url.searchParams.set("gender", gender);
+  name && url.searchParams.set("name", name);
 
-export const getCharacters = cache(
-  async (page: number = 1, gender?: string, name?: string) => {
-    var url = new URL("https://rickandmortyapi.com/api/character/");
-    page && url.searchParams.set("page", page.toString());
-    gender && url.searchParams.set("gender", gender);
-    name && url.searchParams.set("name", name);
+  const res = await fetch(url.href);
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+  const covertToJson = await res.json();
 
-    const res = await fetch(url.href);
-    // The return value is *not* serialized
-    // You can return Date, Map, Set, etc.
-    const covertToJson = await res.json();
-
-    return {
-      ok: res.ok,
-      data: covertToJson,
-    };
-  }
-);
+  return {
+    ok: res.ok,
+    data: covertToJson,
+  };
+};
